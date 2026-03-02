620e8ccaad30d8107d9095768b6180d665af6312
viny-el-mono <vinyelmono@gmail.com>
2026-03-02 02:49:16 -0600

    Camion Avance

--- 2026MVP/Assets/NPCs/CamionNPC-s/Asiento.cs
+++ 2026MVP/Assets/NPCs/CamionNPC-s/Asiento.cs
@@ -1,8 +1,16 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+public enum AsientoOrientacion
+{
+    Derecho,
+    Izquierdo,
+    Atras
+}
 public class Asiento : MonoBehaviour
 {
     public bool disponible = false;
+    public AsientoOrientacion  _Orientation;
     public void Start()
     {
         disponible = true;
--- 2026MVP/Assets/NPCs/CamionNPC-s/Pasajero.cs
+++ 2026MVP/Assets/NPCs/CamionNPC-s/Pasajero.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -16,33 +17,71 @@ public enum Estados
 public class Pasajero : MonoBehaviour
 {
     public Estados _Estados;
-    public Transform[] puntos;
+    public List<Transform> puntos;
     public List<Asiento> _Asientos;
-    
- 
+    public Rigidbody rb;
+    public float velocidad = 2;
+    public float _LaDistanciaAl_Asiento = 0;
+    public GameObject PuntosRecorrido;
+    public Animator animaciones;
+    public Asiento Asiento;
+
     public void Start()
     {
         _Estados = Estados.Parado;
+        rb.MovePosition(Vector3.MoveTowards(rb.position, puntos[0].position, velocidad * Time.fixedDeltaTime));
+        animaciones.Play("Female_Armature|Female_Armature|Female_Armature|TSP_Male_Pose_Sitting_01|Female");
+
     }
+
+
+
+    void FixedUpdate()
+    {
+        animaciones.Play("Walk");
+        BuscoAsiento();
+       
+    }
+
+
+
 
     public void BuscoAsiento()
     {
+        
         _Estados = Estados.BuscandoAsiento;
-        foreach (Asiento asiento in _Asientos)
+        for (int i = 0; i < puntos.Count; i++)
         {
-            if (asiento.disponible)
-            {
-                return;
-            }
+            rb.MovePosition(Vector3.MoveTowards(rb.position, puntos[i].position, velocidad * Time.fixedDeltaTime));
         }
     }
-    public void Camino_Al_Asiento()
+
+    private void OnTriggerEnter(Collider other)
     {
-        for (int i = 0; i < puntos.Length; i++)
+        if (other.CompareTag("Asientos"))
         {
+            switch (Asiento._Orientation)
+            {
+                case AsientoOrientacion.Derecho:
+                    rb.rotation = Quaternion.Euler(0,90,0); 
+                    break;
+                case AsientoOrientacion.Izquierdo:
+                    break;
+                case AsientoOrientacion.Atras:
+                    break;
+                default:
+                    break;
+            }
+            if(Asiento._Orientation==AsientoOrientacion.Izquierdo)
+              {
+
+              }
+            rb.MovePosition(Vector3.MoveTowards(rb.position, _Asientos[0].transform.position, velocidad * Time.fixedDeltaTime));
 
         }
     }
+
+   
     //public void EsperoParada()
     //{
 
