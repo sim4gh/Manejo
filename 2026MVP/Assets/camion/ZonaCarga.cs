74defb1da57005d8feaa37b262357955ea707a62
viny-el-mono <vinyelmono@gmail.com>
2026-03-20 02:02:24 -0600

    Camion Carga y Descarga
    junto a otros fixes de colision

--- 2026MVP/Assets/camion/ZonaCarga.cs
+++ 2026MVP/Assets/camion/ZonaCarga.cs
@@ -1,15 +1,87 @@
+using TMPro;
 using UnityEngine;
 
 public class ZonaCarga : MonoBehaviour
 {
 
-    void Start()
+    public bool camionCargando = false;
+    public bool descargando = false;
+    public string TipoZona;
+    public float contadorPorCaja = 0;
+    public GameObject alertas;
+    public int cajas = 0;
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.CompareTag("CajaCamion"))
+        {
+            if (TipoZona=="ZonaDescarga")
+            {
+                descargando=true;
+            }
+            else
+            {
+                camionCargando = true;
+
+            }
+
+        }
+      
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("CajaCamion"))
+        {
+            camionCargando = false ;
+            descargando = false;
+            alertas.SetActive(false);
+
+        }
     }
 
+    public void Descarga()
+    {
+        contadorPorCaja += Time.deltaTime;
+        if (contadorPorCaja >= 3)
+        {
+
+            cajas--;
+            contadorPorCaja = 0;
+            if (cajas <= 0)
+            {
+                alertas.GetComponent<TextMeshProUGUI>().text = "DESCARGA COMPLETA!";
+                alertas.SetActive(true);
+
+            }
+        }
+    }
+
+    public void Cargando()
+    {
+        contadorPorCaja += Time.deltaTime;
+        if (contadorPorCaja >= 3)
+        {
+           
+            cajas--;
+            contadorPorCaja = 0;
+            if (cajas <= 0)
+            {
+                alertas.GetComponent<TextMeshProUGUI>().text = "CARGADO!";
+                alertas.SetActive(true);
+                
+            }
+        }
+    }
     void Update()
     {
-        
+
+        if (camionCargando)
+        {
+            Cargando();
+        }
+        if (descargando)
+        {
+            Descarga();
+        }
+
     }
 }
