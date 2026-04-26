5ec8acc227aa9142f92826873e6aa80718a9f911
KaroolTheMono <vinyelmono@gmail.com>
2026-04-26 02:44:37 -0600

    Detección de semaforos
    detecta si rebasas una luz roja, y se creó el script Indicaciones para empezar a desarrollar el sistema de navegación por la ciudad

---
+++ 2026MVP/Assets/Custom/Semaforos/Semaforos.cs
@@ -0,0 +1,42 @@
+using Gley.TrafficSystem;
+using UnityEngine;
+
+public class Semaforos : MonoBehaviour
+{
+
+    public bool _SemaforoVerdeActivo = false;
+   
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerComponent>())
+        {
+            foreach(Transform semaforo in transform)
+            {
+                if (semaforo.gameObject.activeSelf)
+                {
+                    if(semaforo.gameObject.name== "GreenLightOn")
+                    {
+                        _SemaforoVerdeActivo = true;
+                        Debug.Log("ADELANTE");
+                    }
+                    else
+                    {
+                        _SemaforoVerdeActivo=false;
+                    }
+                }
+            }
+            
+        }
+    }
+
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerComponent>()&&_SemaforoVerdeActivo==false)
+        {
+            Debug.Log("INFRACCIONNNNNNNNNNNN");
+        }
+    }
+ 
+}
